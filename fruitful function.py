4d390e3abca56a204ff4f20be088135cb222a55c
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-11-17 21:24:57 +0545

    fruitful_function_in_python

---
+++ fruitful function.py
@@ -0,0 +1,16 @@
+#fruitfui function
+from math import *
+def area(r):
+    return pi * r **2    #return value
+print(area(2))
+
+
+def value(x):
+    if x<0:
+        return -x      # multiple return
+    else:
+        return x
+    
+print(value(2))
+
+
